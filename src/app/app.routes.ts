20d8526b7aeae2bb78002dfaf788f84f6cb4d1d2
Kaartikeya Tyagi <techkaartikeya06@gmail.com>
2026-01-28 00:04:53 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ItemCard } from './item-card/item-card';
+import { LoginForm } from './login-form/login-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"/login",component: LoginForm
+    }
+];
--- src/app/student-table/student-table.ts
+++ src/app/student-table/student-table.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-student-table',
